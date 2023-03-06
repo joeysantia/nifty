86840b48faeadee5283b94ba2d54d7352783ed79
Joey Santia <joey.santia@gmail.com>
2023-03-06 15:27:32 -0500

    Add flex containers and FavoriteButton styling to ShopPage

--- src/js/components/ShopPage.js
+++ src/js/components/ShopPage.js
@@ -2,20 +2,26 @@ import FavoriteButton from "./FavoriteButton"
 import ListingBlurb from "./ListingBlurb"
 import ReviewHeader from "./ReviewHeader"
 import ReviewList from "./ReviewList"
+import '../../css/ShopPage.css'
+import { useSelector } from "react-redux"
+import { selectUser } from "../reduxFiles/slices/userSlice"
 
 const ShopPage = ({ shop }) => {
+
+    let user = useSelector(selectUser)
     return (
         <main id="shop-page">
-            <div>
+            <div id="banner-photo-container">
                 {shop.bannerPhotos.map((photo, i) => {
                     return <img key={i} src={photo.src} alt={photo.alt} />
                 })}
             </div>
-            <div>
+            <div className="middle-row">
+                <div></div>
                 <img src={shop.profilePhoto.src} alt={shop.profilePhoto.alt} />
-                <div>
+                <div className="fav-button-container">
                     <FavoriteButton item={shop} />
-                    <span>Follow shop</span>
+                    <span>{user.favoriteShops.includes(shop) ? "Following shop" : "Follow shop"}</span>
                 </div>
             </div>
             <h2>{shop.name}</h2>
@@ -24,7 +30,7 @@ const ShopPage = ({ shop }) => {
             <p>{shop.description}</p>
             <section>
                 {shop.listings.map((listing, i) => {
-                    return <ListingBlurb listing={listing} />
+                    return <ListingBlurb key={i} listing={listing} />
                 })}
             </section>
             <ReviewList reviews={shop.reviews} />
