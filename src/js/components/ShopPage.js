f1060d5db33acddf3b3fd7989170f5181a478ad8
Joey Santia <joey.santia@gmail.com>
2023-03-06 16:13:23 -0500

    Make text & FavoriteButton responsive to user favorites, write corresponding tests

--- src/js/components/ShopPage.js
+++ src/js/components/ShopPage.js
@@ -9,6 +9,14 @@ import { selectUser } from "../reduxFiles/slices/userSlice"
 const ShopPage = ({ shop }) => {
 
     let user = useSelector(selectUser)
+
+    function userHasFavoritedShop() {
+        let userFavShopsString = JSON.stringify(user.favoriteShops)
+        let shopString = JSON.stringify(shop)
+
+        return userFavShopsString.indexOf(shopString) >= 0
+    }
+
     return (
         <main id="shop-page">
             <div id="banner-photo-container">
@@ -21,7 +29,7 @@ const ShopPage = ({ shop }) => {
                 <img src={shop.profilePhoto.src} alt={shop.profilePhoto.alt} />
                 <div className="fav-button-container">
                     <FavoriteButton item={shop} />
-                    <span>{user.favoriteShops.includes(shop) ? "Following shop" : "Follow shop"}</span>
+                    <span>{userHasFavoritedShop() ? "Following shop" : "Follow shop"}</span>
                 </div>
             </div>
             <h2>{shop.name}</h2>
--- src/js/components/tests/ShopPage.test.js
+++ src/js/components/tests/ShopPage.test.js
@@ -1,62 +1,222 @@
-import { render, screen } from "@testing-library/react"
-import ShopPage from "../ShopPage"
+import { render, screen } from "@testing-library/react";
+import { Provider, useDispatch, useSelector } from "react-redux";
+import { editShops } from "../../reduxFiles/slices/shopSlice";
+import { editUser, selectUser } from "../../reduxFiles/slices/userSlice";
+import { store } from "../../reduxFiles/store";
+import ShopPage from "../ShopPage";
+import InitStore from "./utils/InitStore";
 
-jest.mock('../FavoriteButton.js', () => () => {
-    return <h1>Favorite Button</h1>
-})
+jest.mock("../FavoriteButton.js", () => ({ item }) => {
+
+
+    let favoriteShops = [{
+            name: "Homemade Goods",
+            id: "123",
+            type: "shop",
+            profilePhoto: "www.photo.com",
+            bannerPhotos: [
+              {
+                src: "www.photo.com",
+                alt: "banner",
+              },
+              {
+                src: "www.photo.com",
+                alt: "banner",
+              },
+            ],
+            city: "Santa Fe",
+            descritpion: "We make homemade goods",
+            reviews: [
+              {
+                id: "123",
+                userPhoto: "www.photo.com",
+                stars: 4,
+                description: "Great item!",
+                img: {
+                  src: "www.photo.com",
+                  alt: "review",
+                },
+              },
+              {
+                id: "456",
+                userPhoto: "www.photo.com",
+                stars: 5,
+                description: "Love it!",
+                img: {
+                  src: "www.photo.com",
+                  alt: "review",
+                },
+              },
+            ],
+            listings: [
+              {
+                name: "shoes",
+                id: "abcdefg",
+                type: "listing",
+                shopId: "123",
+                price: 29.95,
+                description: "New pair of shoes",
+                options: [],
+                images: [],
+                reviews: [],
+              },
+              {
+                name: "shirt",
+                id: "hijklmn",
+                type: "listing",
+                shopId: "456",
+                price: 19.95,
+                description: "Brand new t-shirt",
+                options: [],
+                images: [],
+                reviews: [],
+              },
+            ],
+          }]
+
+          function userHasFavoritedShop() {
+            let userFavShopsString = JSON.stringify(favoriteShops)
+            let shopString = JSON.stringify(item)
+
+    
+            return userFavShopsString.indexOf(shopString) >= 0
+        }
+    return (
+        <h1>{`This shop is${userHasFavoritedShop() ? "" : " not"} favorited`}</h1>
+    )
+
+});
 
 jest.mock("../ListingBlurb.js", () => () => {
-    return <h1>Listing Blurb</h1>
-})
+  return <h1>Listing Blurb</h1>;
+});
 
 jest.mock("../ReviewHeader.js", () => () => {
-    return <h1>Review Header</h1>
-})
+  return <h1>Review Header</h1>;
+});
 
 jest.mock("../ReviewList.js", () => () => {
-    return <h1>Review Listing</h1>
-})
+  return <h1>Review Listing</h1>;
+});
 
 describe("ShopPage", () => {
-
-    let shop = {
-        name: "Homemade Goods",
+  let shop = {
+    name: "Homemade Goods",
+    id: "123",
+    type: "shop",
+    profilePhoto: "www.photo.com",
+    bannerPhotos: [
+      {
+        src: "www.photo.com",
+        alt: "banner",
+      },
+      {
+        src: "www.photo.com",
+        alt: "banner",
+      },
+    ],
+    city: "Santa Fe",
+    descritpion: "We make homemade goods",
+    reviews: [
+      {
         id: "123",
+        userPhoto: "www.photo.com",
+        stars: 4,
+        description: "Great item!",
+        img: {
+          src: "www.photo.com",
+          alt: "review",
+        },
+      },
+      {
+        id: "456",
+        userPhoto: "www.photo.com",
+        stars: 5,
+        description: "Love it!",
+        img: {
+          src: "www.photo.com",
+          alt: "review",
+        },
+      },
+    ],
+    listings: [
+      {
+        name: "shoes",
+        id: "abcdefg",
+        type: "listing",
+        shopId: "123",
+        price: 29.95,
+        description: "New pair of shoes",
+        options: [],
+        images: [],
+        reviews: [],
+      },
+      {
+        name: "shirt",
+        id: "hijklmn",
+        type: "listing",
+        shopId: "456",
+        price: 19.95,
+        description: "Brand new t-shirt",
+        options: [],
+        images: [],
+        reviews: [],
+      },
+    ],
+  };
+  it("renders as a main element with photos, child components, and shop info", () => {
+    render(
+      <Provider store={store}>
+        <InitStore />
+        <ShopPage shop={shop} />
+      </Provider>
+    );
+    expect(screen).toMatchSnapshot();
+  });
+
+  it("displays text 'Follow shop' and correctly styled FavoriteButton if the shop is not in the user's favorites", () => {
+    
+    let diffShop = {
+        name: "NOT Homemade Goods",
+        id: "456",
         type: "shop",
         profilePhoto: "www.photo.com",
         bannerPhotos: [
-            {
-                src: "www.photo.com",
-                alt: "banner"
-            },
-            {
-                src: "www.photo.com",
-                alt: "banner"
-            }
+          {
+            src: "www.photo.com",
+            alt: "banner",
+          },
+          {
+            src: "www.photo.com",
+            alt: "banner",
+          },
         ],
         city: "Santa Fe",
         descritpion: "We make homemade goods",
-        reviews: [{
-            id: '123',
+        reviews: [
+          {
+            id: "123",
             userPhoto: "www.photo.com",
             stars: 4,
             description: "Great item!",
             img: {
-                src: "www.photo.com",
-                alt: "review"
-            }
-        },
-        {
-            id: '456',
+              src: "www.photo.com",
+              alt: "review",
+            },
+          },
+          {
+            id: "456",
             userPhoto: "www.photo.com",
             stars: 5,
             description: "Love it!",
             img: {
-                src: "www.photo.com",
-                alt: "review"
-            }
-        }],
-        listings: [{
+              src: "www.photo.com",
+              alt: "review",
+            },
+          },
+        ],
+        listings: [
+          {
             name: "shoes",
             id: "abcdefg",
             type: "listing",
@@ -77,10 +237,126 @@ describe("ShopPage", () => {
             options: [],
             images: [],
             reviews: [],
-          },]
+          },
+        ],
       }
-    it('renders as a main element with photos, child components, and shop info', () => {
-        render(<ShopPage shop={shop} />)
-        expect(screen).toMatchSnapshot()
-    })
-})
+    
+    render(
+        <Provider store={store}>
+          <InitStore />
+          <ShopPage shop={diffShop} />
+        </Provider>
+      );
+    expect(screen.getByText("This shop is not favorited")).toBeInTheDocument()
+    expect(screen.getByText("Follow shop")).toBeInTheDocument()
+  })
+
+  it("displays text 'Following shop' and correctly styled FavoriteButton if the shop is not in the user's favorites", () => {
+    
+    function AddShopToFavorites() {
+        let newUser = {
+            name: "John Smith",
+            email: "john.smith@gmail.com",
+            photoURL: "www.photo.com",
+            id: "123abc",
+            cart: [],
+            gender: "male",
+            city: "San Diego",
+            about: "Passionate about arts and crafts!",
+            favoriteItems: [
+              {
+                name: "shirt",
+                id: "hijklmn",
+                type: "listing",
+                shopId: "456",
+                price: 19.95,
+                description: "Brand new t-shirt",
+                options: [],
+                images: [{
+                  src: 'www.photo.com',
+                  alt: "shirt"
+                }],
+                reviews: [],
+              },
+            ],
+            favoriteShops: [{
+                name: "Homemade Goods",
+                id: "123",
+                type: "shop",
+                profilePhoto: "www.photo.com",
+                bannerPhotos: [
+                  {
+                    src: "www.photo.com",
+                    alt: "banner",
+                  },
+                  {
+                    src: "www.photo.com",
+                    alt: "banner",
+                  },
+                ],
+                city: "Santa Fe",
+                descritpion: "We make homemade goods",
+                reviews: [
+                  {
+                    id: "123",
+                    userPhoto: "www.photo.com",
+                    stars: 4,
+                    description: "Great item!",
+                    img: {
+                      src: "www.photo.com",
+                      alt: "review",
+                    },
+                  },
+                  {
+                    id: "456",
+                    userPhoto: "www.photo.com",
+                    stars: 5,
+                    description: "Love it!",
+                    img: {
+                      src: "www.photo.com",
+                      alt: "review",
+                    },
+                  },
+                ],
+                listings: [
+                  {
+                    name: "shoes",
+                    id: "abcdefg",
+                    type: "listing",
+                    shopId: "123",
+                    price: 29.95,
+                    description: "New pair of shoes",
+                    options: [],
+                    images: [],
+                    reviews: [],
+                  },
+                  {
+                    name: "shirt",
+                    id: "hijklmn",
+                    type: "listing",
+                    shopId: "456",
+                    price: 19.95,
+                    description: "Brand new t-shirt",
+                    options: [],
+                    images: [],
+                    reviews: [],
+                  },
+                ],
+              }],
+            favoriteMaterials: "",
+          };
+        let dispatch = useDispatch()
+        dispatch(editUser(newUser))
+    }
+    
+    render(
+        <Provider store={store}>
+          <InitStore />
+          <AddShopToFavorites />
+          <ShopPage shop={shop} />
+        </Provider>
+      );
+    expect(screen.getByText("This shop is favorited")).toBeInTheDocument()
+    expect(screen.getByText("Following shop")).toBeInTheDocument()
+  })
+});
