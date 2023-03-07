910093cbacb4b603a7096a38d22f54cde94f3dc4
Joey Santia <joey.santia@gmail.com>
2023-03-06 20:03:14 -0500

    Write SignInPopup component and tests

---
+++ src/js/components/SignInPopup.js
@@ -0,0 +1,88 @@
+import {
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
+import { useEffect, useState } from "react";
+import { auth } from "../firebaseFiles/firebase.config";
+import popupListener from "../helpers/popupListener";
+
+const SignInPopup = ({ removePopup }) => {
+  const [registeringNewAccount, setRegisteringNewAccount] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [inputsAreValid, setInputsAreValid] = useState(false);
+
+  useEffect(() => {
+    const signInPopup = document.querySelector("#sign-in-popup");
+    document.body.addEventListener("click", (e) =>
+      popupListener(signInPopup, e.target, removePopup)
+    );
+    return () => {
+      document.body.removeEventListener("click", (e) =>
+        popupListener(signInPopup, e.target, removePopup)
+      );
+    };
+  }, []);
+
+  useEffect(() => {
+    let emailRegex = /(?=.*@)(?=.*\.)/;
+    let passwordRegex = /(?=.*[A-Z])(?=.*[0-9]).{8,}/;
+
+    if (
+      emailRegex.test(email) &&
+      passwordRegex.test(password) &&
+      !inputsAreValid
+    ) {
+      setInputsAreValid(true);
+    }
+  }, [email, password]);
+
+  return (
+    <div id="sign-in-popup">
+      <form onSubmit={(e) => signInWithEmailAndPassword(auth, email, password)}>
+        <img src="replace-me" alt="delete" onClick={() => removePopup()} />
+        <div className="top-row">
+          <h2>{registeringNewAccount ? "Create your account" : "Sign in"}</h2>
+          {!registeringNewAccount ? (
+            <button data-testid="register-button-one" onClick={() => setRegisteringNewAccount(true)}>
+              Register
+            </button>
+          ) : null}
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            data-testid="email"
+            required
+            onInput={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="text"
+            name="password"
+            data-testid="password"
+            required
+            onInput={(e) => setPassword(e.target.value)}
+          />
+          <p>Passwords must be at least 8 characters long and contain a capital letter and a number.</p>
+        </div>
+        <button data-testid="register-button-two" type="submit" disabled={!inputsAreValid}>
+          {registeringNewAccount ? "Register" : "Sign In"}
+        </button>
+      </form>
+      <div>
+        <button onClick={() => signInWithPopup(auth, new GoogleAuthProvider())}>
+          <img src="replace-me" alt="google"></img>
+          <span>Continue with Google</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SignInPopup
---
+++ src/js/components/tests/SignInPopup.test.js
@@ -0,0 +1,102 @@
+import { render, screen } from "@testing-library/react";
+import SignInPopup from "../SignInPopup";
+import userEvent from "@testing-library/user-event";
+
+let mockSignInWithEmailAndPassword = jest.fn();
+let mockSignInWithPopup = jest.fn();
+let mockRemovePopup = jest.fn();
+
+jest.mock("firebase/auth", () => {
+  return {
+    ...jest.requireActual("firebase/auth"),
+    signInWithEmailAndPassword: () => mockSignInWithEmailAndPassword(),
+    signInWithPopup: () => mockSignInWithPopup(),
+  };
+});
+
+describe("SignInPopup", () => {
+  it("renders as a container with a form and buttons", () => {
+    render(<SignInPopup removePopup={mockRemovePopup} />);
+    expect(screen).toMatchSnapshot();
+  });
+
+  it("calls removePopup if the delete button is clicked", () => {
+    render(<SignInPopup removePopup={mockRemovePopup} />);
+    userEvent.click(screen.getByAltText("delete"));
+    expect(mockRemovePopup).toHaveBeenCalled();
+  });
+
+  it("calls removePopup if the user clicks outside of the popup", () => {
+    render(
+      <>
+        <SignInPopup removePopup={mockRemovePopup} />
+        <div data-testid="not-sign-in-popup"></div>
+      </>
+    );
+    userEvent.click(screen.getByTestId("not-sign-in-popup"));
+    expect(mockRemovePopup).toHaveBeenCalled();
+  });
+
+  it("renders the login form by default", () => {
+    render(<SignInPopup removePopup={mockRemovePopup} />);
+    expect(
+      screen.getByRole("heading", { name: "Sign in" })
+    ).toBeInTheDocument();
+    expect(screen.getByTestId("register-button-one")).toBeInTheDocument();
+    expect(screen.getByTestId("register-button-two").textContent).toMatch(
+      /sign in/i
+    );
+  });
+
+  it("renders the register form if the Register button is clicked", () => {
+    render(<SignInPopup removePopup={mockRemovePopup} />);
+    userEvent.click(screen.getByRole("button", { name: "Register" }));
+    expect(
+      screen.getByRole("heading", { name: "Create your account" })
+    ).toBeInTheDocument();
+    expect(screen.queryByTestId("register-button-one")).not.toBeInTheDocument();
+    expect(screen.getByTestId("register-button-two").textContent).toMatch(
+      /register/i
+    );
+  });
+
+  it("renders a disabled button by default", () => {
+    render(<SignInPopup removePopup={mockRemovePopup} />);
+    expect(screen.getByTestId('register-button-two')).toBeDisabled()
+});
+
+  it("renders a disabled button if the email input is invalid", () => {
+    render(<SignInPopup removePopup={mockRemovePopup} />);
+    userEvent.type(screen.getByTestId("email"), "john.smith")
+    expect(screen.getByTestId('register-button-two')).toBeDisabled()
+
+  });
+
+  it("renders a disabled button if the password input is invalid", () => {
+    render(<SignInPopup removePopup={mockRemovePopup} />);
+    userEvent.type(screen.getByTestId("password"), "password")
+    expect(screen.getByTestId('register-button-two')).toBeDisabled()
+
+});
+
+  it("renders a functional button if the inputs are valid", () => {
+    render(<SignInPopup removePopup={mockRemovePopup} />);
+    userEvent.type(screen.getByTestId("email"), "peter@email.com")
+    userEvent.type(screen.getByTestId("password"), "Passw0rd")
+    expect(screen.queryByTestId("register-button-one")).not.toBeDisabled();
+  });
+
+  it("calls signInWithEmailAndPassword if the form is submitted with valid inputs", () => {
+    render(<SignInPopup removePopup={mockRemovePopup} />);
+    userEvent.type(screen.getByTestId("email"), "peter@gmail.com")
+    userEvent.type(screen.getByTestId("password"), "Passw0rd")
+    userEvent.click(screen.getByTestId("register-button-two"))
+    expect(mockSignInWithEmailAndPassword).toHaveBeenCalled()
+  });
+
+  it("calls signInWithPopup if user chooses to sign in with Google", () => {
+    render(<SignInPopup removePopup={mockRemovePopup} />);
+    userEvent.click(screen.getByRole("button", { name: "google Continue with Google"}))
+    expect(mockSignInWithPopup).toHaveBeenCalled()
+  });
+}); 
